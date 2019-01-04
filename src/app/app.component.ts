ede327a8a1f362c46ce6416f744e06dd31154386
benlr98 <benlr98@gmail.com>
2019-01-03 23:54:33 -0500

    Talking to parent component with eventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [
+    {type: 'server', name: 'Test Server', content: 'This is a test server'},
     {type: 'server', name: 'Test Server', content: 'This is a test server'}
   ];
+
+  onServerAdded(serverData: {svrName: string, svrContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.svrName,
+      content: serverData.svrContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {bptName: string, bptContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.bptName,
+      content: blueprintData.bptContent
+    });
+  }
+
 }
